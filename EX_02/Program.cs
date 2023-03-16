4cfcc06e8fc350cbe18348864b084a9a606c93eb
DimaSannikov <dimasannikov@gmail.com>
2023-03-17 00:55:24 +0500

    added EX_02

---
+++ EX_02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве
+// находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+void PrintArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows, columns] = rows + columns;
+        }
+    }
+}
+
+Console.Write("Количество строк массива ");
+int countRows = int.Parse(Console.ReadLine());
+
+Console.Write("Количество столбцов массива ");
+int countColumns = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[countRows, countColumns];
+// PrintArray(matrix);
+FillArray(matrix);
+// Console.WriteLine();
+PrintArray(matrix);
